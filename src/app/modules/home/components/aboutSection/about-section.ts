775cdf4fa0be66241a66ace37d7110600dc0d59d
MetinAbbaszade <metinabbaszade@yahoo.com>
2025-07-30 16:31:57 +0400

    finish about component for main page

---
+++ src/app/modules/home/components/aboutSection/about-section.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MatCard } from "@angular/material/card";
+
+@Component({
+  selector: 'app-about-section',
+  imports: [MatCard],
+  templateUrl: './about-section.html',
+  styleUrl: './about-section.css'
+})
+export class AboutSection {
+
+}
--- src/app/modules/home/pages/main/main.ts
+++ src/app/modules/home/pages/main/main.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { Herocomponent } from "../../components/heroComponent/herocomponent";
+import { AboutSection } from "../../components/aboutSection/about-section";
 
 @Component({
   selector: 'app-main',
-  imports: [Herocomponent],
+  imports: [
+    Herocomponent,
+    AboutSection
+  ],
   templateUrl: './main.html',
   styleUrl: './main.css'
 })
