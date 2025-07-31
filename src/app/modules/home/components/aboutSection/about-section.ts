bd7f794488f49c8e65fc5bd446a5a58b2bf3d78a
MetinAbbaszade <metinabbaszade@yahoo.com>
2025-07-31 11:27:15 +0400

    finish contact section

--- src/app/modules/home/components/aboutSection/about-section.ts
+++ src/app/modules/home/components/aboutSection/about-section.ts
@@ -1,13 +1,13 @@
 import { Component, inject } from '@angular/core';
-import { MatCard } from "@angular/material/card";
 import { SharedTranslateModule } from '../../../../shared/modules/shared-translate.module';
 import { TranslateService } from '@ngx-translate/core';
+
 
 @Component({
   selector: 'app-about-section',
   imports: [
-    MatCard,
-    SharedTranslateModule
+    SharedTranslateModule,
+    
   ],
   templateUrl: './about-section.html',
   styleUrl: './about-section.css'
--- src/app/modules/home/components/contactSection/contact-section.ts
+++ src/app/modules/home/components/contactSection/contact-section.ts
@@ -1,16 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 import { MatIconModule } from '@angular/material/icon';
-import { MatFormFieldModule } from '@angular/material/form-field'
+import { TranslateService } from '@ngx-translate/core';
+import { SharedTranslateModule } from '../../../../shared/modules/shared-translate.module';
 
 @Component({
   selector: 'app-contact-section',
   imports: [
     MatIconModule,
-    MatFormFieldModule
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatInputModule,
+    SharedTranslateModule
   ],
   templateUrl: './contact-section.html',
   styleUrl: './contact-section.css'
 })
 export class ContactSection {
+  public translate = inject(TranslateService)
+  form!: FormGroup;
+  aboutData = [
+    {
+      icon: 'location_on',
+      label: 'addressLabel',
+      value: 'addressValue'
+    },
+    {
+      icon: 'call',
+      label: 'phoneLabel',
+      value: 'phoneValue'
+    },
+    {
+      icon: 'message',
+      label: 'emailLabel',
+      value: 'emailValue'
+    },
+  ]
+
+  constructor(
+    public fb: FormBuilder
+  ) { }
+
+  ngOnInit() {
+    this.form = this.fb.group({
+      name: ['', [Validators.minLength(4), Validators.maxLength(10)]],
+      email: ['', [Validators.email]],
+      message: ['', [
+        Validators.required
+      ]]
+    });
+  }
+
+  submitForm() {
+    console.log(this.form.value)
+  }
+
+  get name() {
+    return this.form.get('name');
+  }
+  get email() {
+    return this.form.get('email');
+  }
+
+  get message() {
+    return this.form.get('message')
+  }
 
 }
