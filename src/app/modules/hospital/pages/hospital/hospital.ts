cd6bd567fa786acc9680b2a8a3a2a3855d35cf8b
MetinAbbaszade <metinabbaszade@yahoo.com>
2025-08-15 17:31:51 +0400

    add latest version of hospital

--- src/app/modules/hospital/pages/hospital/hospital.ts
+++ src/app/modules/hospital/pages/hospital/hospital.ts
@@ -6,28 +6,72 @@ import { MatDialog } from '@angular/material/dialog';
 import { ComponentType } from '@angular/cdk/portal';
 import { Hospitaldialog } from '../../components/hospital-dialog/hospitaldialog';
 import { IHospital } from '../../modules/data';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatSelectModule } from '@angular/material/select';
+
 
 @Component({
   selector: 'app-hospital',
   imports: [
     CommonModule,
     RouterLink,
-
+    ReactiveFormsModule,
+    MatIconModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatInputModule,
+    MatDividerModule,
+    MatButtonModule,
+    MatExpansionModule,
+    MatSelectModule
   ],
   templateUrl: './hospital.html',
   styleUrl: './hospital.css'
 })
 export class Hospital {
+  form!: FormGroup;
   hospitalData: any;
+  searchResult: any;
+  filterForm!: FormGroup;
+  click: number = 0;
+
   constructor(
     private HospitalService: HospitalService,
-    private dialog: MatDialog
+    private dialog: MatDialog,
+    private fb: FormBuilder
   ) { }
 
   ngOnInit() {
+    this.searchResult = [];
+    this.fetchData()
+    this.form = this.fb.group({
+      searchData: ''
+    })
+
+    this.filterForm = this.fb.group({
+      speciality: ''
+    })
+  }
+
+  fetchData() {
     this.HospitalService.fetchHospitalData()
       .subscribe((res) => {
+        console.log(res)
         this.hospitalData = res;
+      })
+  }
+
+  filterDatas(filterValue: any) {
+    this.HospitalService.filterData(filterValue.searchData)
+      .subscribe((res) => {
+        console.log(res);
+        this.searchResult = res;
       })
   }
 
@@ -41,6 +85,15 @@ export class Hospital {
       height: '68%',
       data
     })
+  }
+
+  handleSubmit() {
+    this.click += 1
+    if (!this.form.value.searchData) {
+      this.fetchData()
+      return;
+    }
+    this.filterDatas(this.form.value)
   }
 }
 
--- src/app/modules/hospital/services/hospital-service.ts
+++ src/app/modules/hospital/services/hospital-service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { url } from '../../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
+import { filter, finalize, map } from 'rxjs';
+import { IHospital } from '../modules/data';
 
 @Injectable({
   providedIn: 'root'
@@ -15,5 +17,16 @@ export class HospitalService {
 
   fetchHospitalData() {
     return this.http.get(this.baseUrl)
+  }
+
+  filterData(filterData: any) {
+    const data = this.http.get<IHospital[]>(this.baseUrl)
+      .pipe(
+        map(
+          (res: IHospital[]) => res.filter((data: IHospital) => !data.name.toLowerCase().indexOf(filterData))
+        )
+      )
+    console.log(data);
+    return data;
   }
 }
