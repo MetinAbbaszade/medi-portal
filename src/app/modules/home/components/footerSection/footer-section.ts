85f2a5d1783bcd759759bebc44334e1a04553999
MetinAbbaszade <metinabbaszade@yahoo.com>
2025-08-29 11:05:35 +0400

    fix footer component

--- src/app/modules/home/components/footerSection/footer-section.ts
+++ src/app/modules/home/components/footerSection/footer-section.ts
@@ -2,12 +2,15 @@ import { Component, inject } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { SharedTranslateModule } from '../../../../shared/modules/shared-translate.module';
 import { TranslateService } from '@ngx-translate/core';
+import { RouterLink } from '@angular/router';
+import { APP_BASE_HREF } from '@angular/common';
 
 @Component({
   selector: 'app-footer-section',
   imports: [
     MatIconModule,
-    SharedTranslateModule
+    SharedTranslateModule,
+    RouterLink
   ],
   templateUrl: './footer-section.html',
   styleUrl: './footer-section.css'
@@ -16,9 +19,47 @@ export class FooterSection {
 
   public translate = inject(TranslateService)
 
-  quickLinks = ['hospitals', 'doctors', 'about', 'contact', 'login'].map((data) => ({
-    data
-  }))
+  quickLinks = [
+    {
+      label: 'hospitals',
+      href: '/hospital'
+    },
+    {
+      label: 'doctors',
+      href: '/doctor'
+    },
+    {
+      label: 'about',
+      href: '/about'
+    },
+    {
+      label: 'contact',
+      href: '/contact',
+    },
+    {
+      label: 'login',
+      href: '/login'
+    }
+  ]
 
-  services = ['bookAppointment', 'findDoctor', 'emergencyCare', 'insurance'].map((label) => ({label}))
+  // services = ['bookAppointment', 'findDoctor', 'emergencyCare', 'insurance'].map((label) => ({label}))
+
+  services = [
+    {
+      label: 'bookAppointment',
+      href: '/book-appointment'
+    },
+    {
+      label: 'findDoctor',
+      href: '/doctor'
+    },
+    {
+      label: 'emergencyCare',
+      href: '/about'
+    },
+    {
+      label: 'insurance',
+      href: '/contact'
+    }
+  ]
 }
